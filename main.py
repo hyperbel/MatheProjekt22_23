539f275aedfaf9deb41c9bae698edf4109b39add
hyperbel <thomaszimmer524@gmail.com>
2022-12-06 17:51:58 +0100

    main function and renamed Tk() thing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #  numpy, matplotlib, math, sympy, random, time, glob, queue, collections, threading, re , tk
 from tkinter import *
 
-fenster1 = Tk()
 
-fenster1.title("Mathe Projekt 2022")
-fenster1.geometry("350x345")
-fenster1.mainloop()
+def main():
+    root = Tk()
+
+    root.title("Mathe Projekt 2022")
+    root.geometry("350x345")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
