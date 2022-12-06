1d8d0cb22a5901bed5cf2ececfbdfb2f4a423e95
fingadumbledore <fingadumbledore@gmail.com>
2022-12-06 15:38:05 +0100

    Fenster erstellt

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import numpy, matplotlib, math, sympy, random, time, glob, queuem, collections, threadding, re , tk 
+#  numpy, matplotlib, math, sympy, random, time, glob, queuem, collections, threadding, re , tk 
+from tkinter import *
+
+fenster1 = Tk()
+
+fenster1.title("Mathe Projekt 2022")
+fenster1.geometry("350x345")
+fenster1.mainloop()
