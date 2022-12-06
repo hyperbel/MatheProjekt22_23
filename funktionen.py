5c135cd7e5affdf3533aca195f585c16182e67b0
hyperbel <thomaszimmer524@gmail.com>
2022-12-06 18:11:57 +0100

    add linear funktionen button

--- funktionen.py
+++ funktionen.py
@@ -1,1 +1,2 @@
-
+def open_linear_window(root):
+    pass
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 #  numpy, matplotlib, math, sympy, random, time, glob, queue, collections, threading, re , tk
 from tkinter import *
-
+import funktionen as fun
 
 def main():
     root = Tk()
 
-    root.title("Mathe Projekt 2022")
+    get_menu(root)
+    root.title("Mathe-Funktionen-Rechner 2022/23")
     root.geometry("350x345")
     root.mainloop()
 
+def get_menu(root):
+    _menu = Menu(root)
+    _menu.add_command(label="Linear", command=fun.open_linear_window(root))
+    root.config(menu=_menu)
 
 if __name__ == "__main__":
     main()
