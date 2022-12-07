2d775fd7d4a0d01d49edbde1c7130abb34d5a043
hyperbel <thomaszimmer524@gmail.com>
2022-12-07 18:35:59 +0100

    test

---
+++ globals.py
@@ -0,0 +1,4 @@
+# hier keine funktionen rein hauen, nur globale werte, damit die
+# global rumschwimmen und wir die namen weiter benutzen koennen, aber nicht
+# diese werte ueberschreiben
+# btw die datei heisst globals weil global ein python-keyword ist haha
