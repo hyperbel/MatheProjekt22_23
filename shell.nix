63ae3d53958839299e71d9e3a2e84da14bf8438c
hyperbel <hyperbel@users.noreply.github.com>
2022-12-25 20:52:15 +0100

    nix shell & moved plichtenheft

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") {} }:
+
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (ps: [
+      ps.tkinter
+      ps.numpy
+      ps.matplotlib
+      ps.sympy
+    ]))
+
+    pkgs.vim
+    pkgs.sqlite
+    pkgs.tmux
+    pkgs.xpdf
+    pkgs.ganttproject-bin
+  ];
+}
