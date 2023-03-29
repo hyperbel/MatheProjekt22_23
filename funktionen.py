881ef201cad48540cadc491d2cea04a189742a4d
hyperbel <hyperbel@users.noreply.github.com>
2023-03-29 16:20:56 +0200

    add more selfs

--- funktionen.py
+++ funktionen.py
@@ -248,7 +248,7 @@ class Trigonometrische(Frame):
         # figure for matplotlib to plot lines on
         self.fig = plt.Figure(figsize=(10, 10), dpi=100)
 
-        # canvas-like thing for actually drawing
+        # canvas-like thing for actually draselfg
         self.ax = self.fig.add_subplot()
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
@@ -291,10 +291,35 @@ class Trigonometrische(Frame):
 
 
 class Integralrechnung(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.create_widgets()
 
-     def integral_ausrechnen(self):
+    def create_widgets(self):
+        self.funktion_label =  Label(self, text="Funktion:")
+        self.funktion_entry = Entry(self)
+        self.anfangX_label = Label(self, text="Anfang von X:")
+        self.anfangX_entry = Entry(self)
+        self.endeX_label = Label(self, text="Ende von X:")
+        self.endeX_entry = Entry(self)  
+
+        self.y_label = Label(self, text="Y:")
+        self.y_entry = Entry(self)
+        self.loesung_label = Label(self, text="Lösung")
+        self.loesung_entry = Entry(self)
+
+        fig = plt.Figure(figsize=(10, 10), dpi=100)
+
+        # canvas-like thing for actually drawing
+        self.ax = fig.add_subplot()
+
+        # range of numhers, see numpy.org for doc on arange
+        # put canvas onto tk window
+        self.canvas = FigureCanvasTkAgg(fig, master=self)
+
+    def integral_ausrechnen(self):
         # Hohle Werte
-        funktion_text = self.function_entry.get()
+        funktion_text = self.funktion_entry.get()
         anfangx = float(self.anfangX_entry.get())
         endex = float(self.endeX_entry.get())
         y = int(self.y_entry.get())
@@ -313,16 +338,16 @@ class Integralrechnung(Frame):
 
         # Rundet das Ergebnis und gibt es in der Box aus
         ergebnis = round(integral)
-        loesung_entry.delete(0, END)
-        loesung_entry.insert(0, str(ergebnis))
+        self.loesung_entry.delete(0, END)
+        self.loesung_entry.insert(0, str(ergebnis))
 
 
         self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
-        ax.clear()
-        ax.plot(x, y)
-        ax.fill_between(x, y, 0, alpha=0.2)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_title("Integral")
+        self.ax.clear()
+        self.ax.plot(x, y)
+        self.ax.fill_between(x, y, 0, alpha=0.2)
+        self.ax.set_xlabel('x')
+        self.ax.set_ylabel('y')
+        self.ax.set_title("Integral")
         self.canvas.draw()
