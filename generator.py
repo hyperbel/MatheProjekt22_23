f8a20357880f5bcc82cbe4ed756c4fd9e15b730f
hyperbel <hyperbel@users.noreply.github.com>
2023-03-31 12:03:12 +0200

    slightly cleaner code

--- generator.py
+++ generator.py
@@ -8,6 +8,7 @@ def insert_functions(num_functions):
     userid = 1
     
     for _ in range(num_functions):
+        function = ""
         a = random.randint(-10, 10)
         b = random.randint(-10, 10)
         c = random.randint(-10, 10)
@@ -18,142 +19,108 @@ def insert_functions(num_functions):
                 if b > 0:
                     if c > 0:
                         function = f"{a}x^{hoch}+{b}x+{c}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                         function = f"{a}x^{hoch}+{b}x{c}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                 else:
                     if c > 0:
                         function = f"{a}x^{hoch}{b}x+{c}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                         function = f"{a}x^{hoch}{b}x{c}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
             elif hoch == 3:
                 if b > 0:
                     if c > 0:
                         if d > 0:
                             function = f"{a}x^{hoch}+{b}x^2+{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}+{b}x^2+{c}x{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                         function = f"{a}x^{hoch}+{b}x^2{c}x+{d}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                 else:
                     if c > 0:
                         if d > 0:
                             function = f"{a}x^{hoch}{b}x^2+{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}+{b}x^2+{c}x{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                         function = f"{a}x^{hoch}{b}x^2{c}x+{d}"
-                        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
             elif hoch == 4:
                  hoch2 = random.randint(2, 3)
                  if b > 0:
                     if c > 0:
                         if d > 0:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                             function = f"{a}x^{hoch}+{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                  else:
                         if c > 0:
                             if d > 0:
                                 function = f"{a}x^{hoch}{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                             else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
             elif hoch == 5:
                  hoch2 = random.randint(2, 4)
                  if b > 0:
                     if c > 0:
                         if d > 0:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                             function = f"{a}x^{hoch}+{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                  else:
                         if c > 0:
                             if d > 0:
                                 function = f"{a}x^{hoch}{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                             else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
             elif hoch == 6:
                  hoch2 = random.randint(2, 5)
                  if b > 0:
                     if c > 0:
                         if d > 0:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                             function = f"{a}x^{hoch}+{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                  else:
                         if c > 0:
                             if d > 0:
                                 function = f"{a}x^{hoch}{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                             else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
             elif hoch == 7:
                  hoch2 = random.randint(2, 6)
                  if b > 0:
                     if c > 0:
                         if d > 0:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                     else:
                             function = f"{a}x^{hoch}+{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                  else:
                         if c > 0:
                             if d > 0:
                                 function = f"{a}x^{hoch}{b}x^{hoch2}+{c}x+{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                             else:
                                 function = f"{a}x^{hoch}+{b}x^{hoch2}+{c}x{d}"
-                                cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                         else:
                             function = f"{a}x^{hoch}{b}x^{hoch2}{c}x+{d}"
-                            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
                 
            
         else:
             function = f"{a}x + {b}"
-            cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
-        
+
+        cur.execute("INSERT INTO funktionen VALUES (?, ?)", (function, userid))
         yield function
     
     con.commit()
