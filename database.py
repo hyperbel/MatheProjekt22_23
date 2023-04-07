8ff872be6fbb35146570bdb1eab4aebf3f0b43a9
fingadumbledore <fingadumbledore@gmail.com>
2023-04-07 15:01:10 +0200

    datenbank erstell richtig tabelle

--- database.py
+++ database.py
@@ -2,27 +2,31 @@ import sqlite3
 
 def create_db():
     """ Erstellt Datenbank """
-    con = sqlite3.connect("mathe.db")
-    cur = con.cursor()
+    try:
+        con = sqlite3.connect("mathe.db")
+        cur = con.cursor()
 
-    # Tabelle user erstellen
+        # Tabelle user erstellen
 
-    # MM-DD-YYYY HH:MM
-    sql = "CREATE TABLE user (" \
-          "username TEXT, "  \
-          "passwort TEXT);"
-    cur.execute(sql)
-    con.commit()
+        # MM-DD-YYYY HH:MM
+        sql = "CREATE TABLE user (" \
+              "username TEXT, "  \
+              "passwort TEXT);"
+        cur.execute(sql)
+        con.commit()
 
-    # Tabelle funktionen erstellen
+        # Tabelle funktionen erstellen
 
-    sql = "CREATE TABLE funktionen (" \
-          "funktion TEXT, "	\
-          "zeit TEXT, "	\
-          "userid INTEGER);"
-    cur.execute(sql)
-    con.commit()
-    con.close()
+        sql = "CREATE TABLE funktionen (" \
+              "funktion TEXT, "    \
+              "zeit TEXT, "    \
+              "userid INTEGER);"
+        cur.execute(sql)
+        con.commit()
+        con.close()
+        print("Datenbank erfolgreich erstellt!")
+    except Exception as e:
+        print("Fehler beim Erstellen der Datenbank: ", e)
 
 if __name__ == "__main__":
     create_db()
