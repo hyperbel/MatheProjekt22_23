047298974fceab58b536a7667d2e27284b20cb90
hyperbel <hyperbel@users.noreply.github.com>
2023-04-10 10:35:37 +0200

    adjusted height for buttons in terme_eingeben

--- oop.py
+++ oop.py
@@ -18,8 +18,8 @@ class MainWindow(Tk):
     def __init__(self):
         super().__init__()
         self.title("Mathe Projekt 22/23")
-        self.minsize(1200, 800)
-        self.maxsize(1200, 800)
+        self.minsize(1200, 850)
+        self.maxsize(1200, 850)
 
         self.create_menu()
         self.config(menu=self.menu)
