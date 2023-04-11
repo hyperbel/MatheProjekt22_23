7a2fc87168b75435419a3b829692593d976604a7
fingadumbledore <fingadumbledore@gmail.com>
2023-04-11 08:30:55 +0200

    verlauf löschen

--- verlauf.py
+++ verlauf.py
@@ -1,5 +1,6 @@
 import sqlite3
-from tkinter import Frame, Listbox, Scrollbar, END, BOTH, LEFT, RIGHT, ttk, BOTTOM
+import time
+from tkinter import Frame, Listbox, Scrollbar, END, BOTH, LEFT, RIGHT, ttk, BOTTOM, messagebox
 
 
 class Verlauf(Frame):
@@ -25,8 +26,18 @@ class Verlauf(Frame):
         #print(ergebnis)
         return ergebnis
 
-    def remove_verlauf (self, userid: int, zeit: str):
-        pass
+    
+    def remove_verlauf(self, userid: int, minu: str, std: str):
+        try:
+            aktuelle_zeit = time.strftime("%H:%M:%S")
+            zeit = time.strftime("%H:%M:%S", time.strptime(f"{std}:{minu}:00", "%H:%M:%S"))
+            aktuelle_zeit_in_sec = sum(x * int(t) for x, t in zip([3600, 60, 1], aktuelle_zeit.split(":")))
+            zeit_in_sec = sum(x * int(t) for x, t in zip([3600, 60, 1], zeit.split(":")))
+            alte_zeit_in_sec = aktuelle_zeit_in_sec - zeit_in_sec
+            alte_zeit = time.strftime("%H:%M:%S", time.gmtime(alte_zeit_in_sec))
+            sql = f"DELETE FROM funktionen WHERE zeit BETWEEN '{alte_zeit}' AND '{aktuelle_zeit}' AND userid = '1';"
+        except:
+            _ = messagebox.showwarning(title="Fehler", message="Fehler beim Löschen")
 
     def scrollbar_init(self):
         self.scrollbar = Scrollbar(self, width=10)
@@ -52,9 +63,24 @@ class Verlauf(Frame):
             self.listbox.delete(0, END)
             self.cur.execute("DELETE FROM funktionen;")
             self.con.commit()
+        elif selection == '5 min':
+            self.remove_verlauf(1,'05',0)
+        elif selection == '10 min':
+           self.remove_verlauf(1,10,0)
+        elif selection == '15 min':
+           self.remove_verlauf(1,15,0)
+        elif selection == '30 min':
+            self.remove_verlauf(1,30,0)
+        elif selection == '1 std':
+            self.remove_verlauf(1,0,1)
+        elif selection == '2 std':
+            self.remove_verlauf(1,0,2)
+        elif selection == '8 std':
+            self.remove_verlauf(1,0,8)
+        elif selection == '24 std':
+            self.remove_verlauf(1,0,24)
         else:
-            # MM-DD-YYYY HH:MM
-            self.remove_verlauf(1, "03.30.2023 09:30")
+           _ = messagebox.showinfo(title="Auswahl ungültig", message="Sie müssen in der Combobox etwas auswählen")
 
     def delete_frame_init(self):
         self.delete_frame = Frame(self)
