b9426d4e69856144fb142fda88c536ee0038d904
fingadumbledore <fingadumbledore@gmail.com>
2023-04-12 12:42:08 +0200

    dsa ist ein Versuch

--- oop.py
+++ oop.py
@@ -14,11 +14,32 @@ def import_funktionen():
     import funktionen as _f
     f = _f
 
+class WelcomeFrame(Frame):
+    def __inít__(self, master):
+        super().__init__()
+
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.verlauf_hilfe = Label(self, text="Links ist der Ferlauf zu sehen. Dort werden Funktionen eingefügt, wenn Sie das Graph sehen!")
+        self.terme_eingeben_hilfe = Label(self, text="Im Terme-Eingeben-Teil können sie eine Ganzrationale Funktion eingeben!")
+        self.exponential_hilfe = Label(self, text="Hier können Sie sehen, wie eine exponentielle Funktion aussieht!")
+        self.trigonometrische_hilfe = Label(self, text="bitte noch einfügen") # TODO
+        self.integral_hilfe = Label(self, text="bitte noch einfügen") #TODO
+
+        self.show_labels()
+
+    def show_labels():
+        self.verlauf_hilfe.grid(row=0, column=0)
+        self.terme_eingeben_hilfe.grid(row=1, column=0)
+        self.exponential_hilfe.grid(row=2, column=0)
+        self.trigonometrische_hilfe.grid(row=3, column=0)
+        self.integral_hilfe.grid(row=4, column=0)
+
+
 class MainWindow(BaseWindow):
     def __init__(self):
         super().__init__()
-      
-
 
         self.create_menu()
         self.config(menu=self.menu)
@@ -30,7 +51,11 @@ class MainWindow(BaseWindow):
         self.verlauf = Verlauf(self, self)
         self.verlauf.grid(row=0, column=0, sticky="nsew")
 
-        self.selected_frame = None
+        self.selected_frame = WelcomeFrame(self)
+
+        self.selected_frame.config(height=self.winfo_height())
+        self.selected_frame.grid(row=0, column=1, sticky="nsew")
+
 
     def get_verlauf(self) -> Verlauf:
         return self.verlauf
