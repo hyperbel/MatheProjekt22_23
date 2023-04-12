d5ee09e2137ef7d2b7d7d9f9599cb937fbf005fe
fingadumbledore <fingadumbledore@gmail.com>
2023-04-12 06:58:54 +0200

    das brauchen wir glaube nicht mehr

--- oop.py
+++ oop.py
@@ -14,8 +14,6 @@ def import_funktionen():
     import funktionen as _f
     f = _f
 
-
-
 class MainWindow(BaseWindow):
     def __init__(self):
         super().__init__()
@@ -101,83 +99,6 @@ class MainWindow(BaseWindow):
         account_menu.add_command(label="Logout", command=self.logout)
         account_menu.add_command(label="Löschen", command=self.loeschen)
         self.menu.add_cascade(label="Account", menu=account_menu)
-
-        
-
-        # funktionen_menu.add_command(label="Trigonometrische", command=open_trigonometrische_window)
-        # # Differenzial Rechnen = Ableitungen, deshalb wir dies in dem anderen Fenster sein
-        # funktionen_menu.add_command(label="Integralrechnung", command=open_integralrechnung_window)
-        # _menu.add_cascade(label="Funktionen", menu=funktionen_menu)
-
-        # account_menu.add_command(label="Logout", command=self.destroy)
-        # _menu.add_cascade(label="Account", menu=account_menu)
-
-        # hilfe_menu.add_command(label="hilfe", command=open_hilfe_window)
-        #_menu.add_cascade(label="Hilfe", menu=hilfe_menu)
-        # hilfe_menu.add_command(label="über", command=open_ueber_window)
-
-class WelcomeWizard:
-    def __init__(self, master):
-        self.master = master
-        master.title("Willkommen")
-
-        self.pages = []
-        self.current_page = 0
-
-        page1 = Frame(master)
-        label1 = Label(page1, text="Willkommen beim Mathefunktionsrechner!")
-        label1.pack()
-        self.pages.append(page1)
-
-        page2 = Frame(master)
-        label2 = Label(page2, text="Diese Software kann dir mathematische Funktionen berechnen und erklären.")
-        label2.pack()
-        self.pages.append(page2)
-
-        page3 = Frame(master)
-        label3 = Label(page3, text="Wenn du hilfe brauchst klicke einfach auf das ? im Fenster")
-        label3.pack(side="left")
-        self.finish_button = Button(page3, text="Finish", command=self.finish)
-        self.finish_button.pack(side="left")
-        self.pages.append(page3)
-
-        button_frame = Frame(master)
-        self.back_button = Button(button_frame, text="Back", command=self.back)
-        self.back_button.pack(side="left")
-        self.next_button = Button(button_frame, text="Next", command=self.next)
-        self.next_button.pack(side="left")
-
-        self.show_current_page()
-        button_frame.pack()
-
-    def show_current_page(self):
-        self.pages[self.current_page].pack()
-        if self.current_page == 0:
-            self.next_button.pack()
-        elif self.current_page == len(self.pages) - 1:
-            self.back_button.pack()
-            self.finish_button.pack(side="left")
-            self.next_button.pack_forget()
-        else:
-            self.back_button.pack()
-            self.next_button.pack()
-
-    def hide_current_page(self):
-        self.pages[self.current_page].pack_forget()
-
-    def next(self):
-        self.hide_current_page()
-        self.current_page += 1
-        self.show_current_page()
-
-    def back(self):
-        self.hide_current_page()
-        self.current_page -= 1
-        self.show_current_page()
-
-    def finish(self):
-        self.master.destroy()
-
 
 class Splash(Tk):
     def __init__(self):
